93fcef7831eae91bde1ce9f3173c19f369993cc8
Hans Johnson <hans-johnson@uiowa.edu>
2020-03-01 15:50:17 -0600

    COMP: Match template style more closely.

--- itk-module.cmake
+++ itk-module.cmake
@@ -13,34 +13,23 @@ file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
 # define the dependencies of the include module and the tests
 set(ModuleName "SmoothingRecursiveYvvGaussianFilter")
 if(ITK_USE_GPU)
-    itk_module(${ModuleName}
-     DEPENDS
-        ITKCommon
-        ITKIOImageBase
-        ITKImageFilterBase
-        ITKSmoothing
-        ITKGPUSmoothing
-        ITKGPUCommon
-     TEST_DEPENDS
-        ITKTestKernel #to handle IO in src
-        ITKGPUCommon
-        ITKSmoothing
-     EXCLUDE_FROM_DEFAULT
-     DESCRIPTION
-         "${DOCUMENTATION}"
-    )
-else()
-    itk_module(${ModuleName}
-     DEPENDS
-        ITKCommon
-        ITKIOImageBase
-        ITKImageFilterBase
-        ITKSmoothing
-     TEST_DEPENDS
-        ITKTestKernel #to handle IO in src
-        ITKSmoothing
-     EXCLUDE_FROM_DEFAULT
-     DESCRIPTION
-         "${DOCUMENTATION}"
-    )
+  set(${ModuleName}_GPU_DEPENDANCIES "ITKGPUSmoothing")
+  set(${ModuleName}_GPU_COMMON       "ITKGPUCommon")
 endif()
+itk_module(${ModuleName}
+ DEPENDS
+    ITKCommon
+    ITKIOImageBase
+    ITKImageFilterBase
+    ITKSmoothing
+    ${${ModuleName}_GPU_DEPENDANCIES} 
+    ${${ModuleName}_GPU_COMMON}
+ TEST_DEPENDS
+    ITKTestKernel #to handle IO in src
+    ${${ModuleName}_GPU_COMMON}
+    ITKSmoothing
+ DESCRIPTION
+     "${DOCUMENTATION}"
+ EXCLUDE_FROM_DEFAULT
+ # Header only library does not use ENABLE_SHARED
+)
